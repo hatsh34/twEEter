4bcef80b7b07d506734e9b5c848ff327637d0add
harsh <harshsince2003@gmail.com>
2025-03-11 13:34:01 +0530

    lesgo

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import {v2 as cloudinary} from "cloudinary";
 import postRoutes from "./routes/post.route.js";
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js";
+import notificationRoutes from "./routes/notification.route.js";
  
 
 
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes)
+app.use("/api/notifications", notificationRoutes);
 
  
     
